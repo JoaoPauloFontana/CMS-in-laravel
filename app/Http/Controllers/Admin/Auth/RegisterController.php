395525c21142e64dedcf6d47ece49c5376b729ba
Shaiqna <jpfontana12@icloud.com>
2022-04-27 22:29:05 -0300

    Added registration method

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -45,8 +47,25 @@ class RegisterController extends Controller
         return view('admin.register');
     }
 
-    public function register(){
+    public function register(Request $request){
+        $data = $request->only([
+            'name',
+            'email',
+            'password',
+            'password_confirmation'
+        ]);
 
+        $validator = $this->validator($data);
+
+        if ($validator->fails()) {
+            return redirect()->route('register')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $user = $this->create($data);
+        Auth::login($user);
+        return redirect()->route('admin');
     }
 
     /**
@@ -58,8 +77,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'string', 'email', 'max:100', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
