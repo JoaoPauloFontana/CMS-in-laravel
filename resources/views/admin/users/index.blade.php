5d26db8a5ab2497166b8ef00a3edbaa40550da42
Shaiqna <jpfontana12@icloud.com>
2022-04-28 10:21:50 -0300

    Added user list controller and view

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,9 +3,27 @@
 @section('title', 'Usuários')
 
 @section('content_header')
-    <h1>Meus Usuários</h1>
+    <h1>Meus Usuários:</h1>
 @endsection
 
 @section('content')
-    ...
+    <table class="table table-hover">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+        </tr>
+        @foreach ($users as $user)
+            <tr>
+                <th>{{ $user->id }}</th>
+                <th>{{ $user->name }}</th>
+                <th>{{ $user->email }}</th>
+                <th>
+                    <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-info">Editar</a>
+                    <a href="{{ route('users.destroy', ['user' => $user->id]) }}" class="btn btn-sm btn-danger">Excluir</a>
+                </th>
+            </tr>
+        @endforeach
+    </table>
 @endsection
