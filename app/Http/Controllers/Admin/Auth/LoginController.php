1ece99d0b78cabf8e027c88a1f077691e82c403b
Shaiqna <jpfontana12@icloud.com>
2022-04-28 09:38:22 -0300

    Added validations and error messages

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -45,20 +46,40 @@ class LoginController extends Controller
     }
 
     public function authenticate(Request $request){
-        $creds = $request->only('email', 'password');
+        $data = $request->only([
+            'email',
+            'password',
+            'remember'
+        ]);
 
+        $validator = $this->validator($data);
 
-        if(Auth::attempt($creds)){
-            return redirect()->route('admin');
-        }else{
+        if($validator->fails()){
             return redirect()->route('login')
-                ->with('warning', 'Email e/ou senha inválidos');
+                ->withErrors($validator)
+                ->withInput();
         }
 
+        if(Auth::attempt($data)){
+            return redirect()->route('admin');
+        }else{
+            $validator->errors()->add('password', 'Email e/ou senha incorretos!');
+
+            return redirect()->route('login')
+                ->withErrors($validator)
+                ->withInput();
+        }
     }
 
     public function logout(){
         Auth::logout();
         return redirect()->route('login');
+    }
+
+    protected function validator(array $data){
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string', 'min:8']
+        ]);
     }
 }
