5a068ff1d92c6d30a714d4db78f56fe2fea37eac
Shaiqna <jpfontana12@icloud.com>
2022-04-28 23:25:59 -0300

    Added error class in input

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -23,25 +23,25 @@
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">Nome Completo</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" placeholder="Nome Completo" name="name" value="{{ old('name') }}">
+                <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Nome Completo" name="name" value="{{ old('name') }}">
             </div>
         </div>
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}">
             </div>
         </div>
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">Senha</label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" placeholder="Senha" name="password">
+                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Senha" name="password">
             </div>
         </div>
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">Confirmar Senha</label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" placeholder="Confirmar Senha" name="password_confirmation">
+                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Confirmar Senha" name="password_confirmation">
             </div>
         </div>
         <div class="form-group row">
