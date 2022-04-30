5861d1f2923fade771053fbdfe17e7dff69ea389
Shaiqna <jpfontana12@icloud.com>
2022-04-30 19:29:32 -0300

    Adding User Level

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('can:edit-users');
     }
     /**
      * Display a listing of the resource.
