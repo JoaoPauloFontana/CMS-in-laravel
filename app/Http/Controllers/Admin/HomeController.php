5e589664bef710c95a8befc0af648012a30eb715
Shaiqna <jpfontana12@icloud.com>
2022-05-01 13:14:35 -0300

    Added dashboard information

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Visitor;
+use App\Models\Page;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -12,8 +15,41 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(){
-        return view('admin.home');
+    public function index()
+    {
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+
+        $visitsCount = Visitor::count();
+
+        $dateLimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')->where('date_access', '>=', $dateLimit)->groupBy('ip')->get();
+        $onlineCount = count($onlineList);
+
+        $pageCount = Page::count();
+
+        $userCount = User::count();
+
+        $pagePie = [];
+        $visitsAll = Visitor::selectRaw('page, count(page) as c')->groupBy('page')->get();
+
+        foreach($visitsAll as $visit){
+            $pagePie[$visit['page']] = intval($visit['c']);
+        }
+
+        $pageLabels = json_encode(array_keys($pagePie));
+        $pageValues = json_encode(array_values($pagePie));
+
+        return view('admin.home', [
+            'visitsCount' => $visitsCount,
+            'onlineCount' => $onlineCount,
+            'pageCount' => $pageCount,
+            'userCount' => $userCount,
+            'pageLabels' => $pageLabels,
+            'pageValues' => $pageValues
+        ]);
     }
 }
 
