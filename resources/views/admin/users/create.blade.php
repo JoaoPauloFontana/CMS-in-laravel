7270c72f550eee9e2e59a997e91069c6d538d9fd
Shaiqna <jpfontana12@icloud.com>
2022-05-01 00:58:14 -0300

    Added card class

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -18,36 +18,44 @@
         </div>
     @endif
 
-    <form action="{{ route('users.store') }}" method="POST" class="form-horizontal">
-        @csrf
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Nome Completo</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Nome Completo" name="name" value="{{ old('name') }}">
-            </div>
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('users.store') }}" method="POST" class="form-horizontal">
+                @csrf
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Nome Completo</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror"
+                            placeholder="Nome Completo" name="name" value="{{ old('name') }}">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email"
+                            name="email" value="{{ old('email') }}">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Senha" name="password">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Confirmar Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Confirmar Senha" name="password_confirmation">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <input type="submit" value="Cadastrar" class="btn btn-success">
+                    </div>
+                </div>
+            </form>
         </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
-                <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Senha</label>
-            <div class="col-sm-10">
-                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Senha" name="password">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Confirmar Senha</label>
-            <div class="col-sm-10">
-                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Confirmar Senha" name="password_confirmation">
-            </div>
-        </div>
-        <div class="form-group row">
-            <div class="col-sm-10">
-                <input type="submit" value="Cadastrar" class="btn btn-success">
-            </div>
-        </div>
-    </form>
+    </div>
 @endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,31 +10,37 @@
 @endsection
 
 @section('content')
-    <table class="table table-hover">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Ações</th>
-        </tr>
-        @foreach ($users as $user)
-            <tr>
-                <th>{{ $user->id }}</th>
-                <th>{{ $user->name }}</th>
-                <th>{{ $user->email }}</th>
-                <th>
-                    <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-info">Editar</a>
-                    @if($loggedId !== intval($user->id))
-                        <form class="d-inline" action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir esse usuário?')">
-                            @method('DELETE')
-                            @csrf
-                            <button class="btn btn-sm btn-danger">Excluir</button>
-                        </form>
-                    @endif
-                </th>
-            </tr>
-        @endforeach
-    </table>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-hover">
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Ações</th>
+                </tr>
+                @foreach ($users as $user)
+                    <tr>
+                        <th>{{ $user->id }}</th>
+                        <th>{{ $user->name }}</th>
+                        <th>{{ $user->email }}</th>
+                        <th>
+                            <a href="{{ route('users.edit', ['user' => $user->id]) }}"
+                                class="btn btn-sm btn-info">Editar</a>
+                            @if ($loggedId !== intval($user->id))
+                                <form class="d-inline" action="{{ route('users.destroy', ['user' => $user->id]) }}"
+                                    method="POST" onsubmit="return confirm('Tem certeza que deseja excluir esse usuário?')">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-sm btn-danger">Excluir</button>
+                                </form>
+                            @endif
+                        </th>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
 
     {{ $users->links('pagination::bootstrap-4') }}
 
